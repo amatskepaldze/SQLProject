c4ea25c6b1949ff5869fe0b611acac663a9cfd98
YaAlex <77159537+YaAlex@users.noreply.github.com>
2021-05-26 18:27:37 +0300

    26.05.21

--- static/scripts/animations.js
+++ static/scripts/animations.js
@@ -1,3 +1,20 @@
-document.querySelector('.control').addEventListener('click', ()=>{
-  document.querySelectorAll('.animation_blocks > span').forEach(el => el.classList.toggle("animation"))
-});
+let a=1;
+function change(){
+    if (a==1){
+        for (let z=1;z<4;z++){
+            let element = document.getElementById("i"+z);
+            element.classList.remove("progress"+z);
+            element.style.visibility='hidden';
+        }
+        let b = document.getElementById("button");
+        a=0;
+    }else if (a==0){
+        for (let z=1;z<4;z++){
+            let element = document.getElementById("i"+z);
+            element.classList.add("progress"+z);
+            element.style.visibility='visible';
+        }
+        let b = document.getElementById("button");
+        a=1;
+    }
+}
