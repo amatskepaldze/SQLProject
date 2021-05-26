aa87bda43bc8e44a713feb3a0786ebb0fedc05cf
YaAlex <77159537+YaAlex@users.noreply.github.com>
2021-05-26 19:37:15 +0300

    26.05.21 кнопка анимаций и cookies

--- static/scripts/animations.js
+++ static/scripts/animations.js
@@ -1,4 +1,5 @@
-let a=1;
+function setcookie(a,b,c) {if(c){var d = new Date();d.setDate(d.getDate()+c);}if(a && b) document.cookie = a+'='+b+(c ? '; expires='+d.toUTCString() : '');else return false;}
+function getcookie(a) {var b = new RegExp(a+'=([^;]){1,}');var c = b.exec(document.cookie);if(c) c = c[0].split('=');else return false;return c[1] ? c[1] : false;}
 function change(){
     if (a==1){
         for (let z=1;z<4;z++){
@@ -6,15 +7,21 @@ function change(){
             element.classList.remove("progress"+z);
             element.style.visibility='hidden';
         }
-        let b = document.getElementById("button");
-        a=0;
+        document.cookie = 'animation=0; path=/';
+        a = getcookie('animation');
     }else if (a==0){
         for (let z=1;z<4;z++){
             let element = document.getElementById("i"+z);
             element.classList.add("progress"+z);
             element.style.visibility='visible';
         }
-        let b = document.getElementById("button");
-        a=1;
+        document.cookie = 'animation=1; path=/';
+        a = getcookie('animation');
     }
+}
+
+let a = getcookie('animation');
+if (a != 1){
+    a = 1;
+    change();
 }
